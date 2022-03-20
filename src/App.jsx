9f4e9144c494fa75684c0a47f24433ea455c1834
raunak_k02 <raunakk728@gmail.com>
2022-03-20 20:43:50 +0530

    completed start page UI

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import StartPage from "./components/StartPage";
+import "./styles.css";
+
+function App() {
+	return (
+		<>
+			<StartPage />
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/StartPage.jsx
@@ -0,0 +1,11 @@
+function StartPage() {
+	return (
+		<div className="start-page">
+			<h1>Quizzical</h1>
+			<p>A quiz taking app</p>
+			<button>Start Quiz</button>
+		</div>
+	);
+}
+
+export default StartPage;
